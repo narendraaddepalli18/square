a67b2e96e258d295f8c511495211384f5eef0b6a
Addepalli Narendra kumar <narendrakumaraddepalli18@gmail.com>
2025-07-12 20:09:11 +0530

    Add files via upload

---
+++ squares.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Squares {
+    public static void main(String args[]){
+        int num;
+        Scanner S=new Scanner(System.in);
+        System.out.println("enter number to generate square");
+        num=S.nextInt();
+int square=num*num;
+System.out.println("squares of the given number :"+square);
+ }
+    
+}
